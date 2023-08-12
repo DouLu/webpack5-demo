b2bf953da8d428729b5484f2d65c86e81b4c6d95
DouLu <luckyludou@gmail.com>
2023-08-12 16:04:53 +0800

    style: add husky

--- src/pages/blog/user/index.tsx
+++ src/pages/blog/user/index.tsx
@@ -1,3 +1,12 @@
 export default function Blog() {
-  return <div>blog</div>;
+  const list = Array(10).fill(1);
+  const test = 12,
+    a = "a";
+  return (
+    <div>
+      {list.map((i, index) => (
+        <li key={i + index}>{i + index}</li>
+      ))}
+    </div>
+  );
 }
